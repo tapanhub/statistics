e5d8bea607acea5bcf65b9366e5e87ecad7d8038
Tapan Mishra <tapanigit@gmail.com>
2020-08-20 00:01:58 +0530

    Adding box plot & few pandas statistics
    Signed-off-by: Tapan Mishra <tapanigit@gmail.com>

--- pystatistics.py
+++ pystatistics.py
@@ -1,10 +1,18 @@
 import statistics
+from scipy import stats
+import pprint
+import pandas as pd
 
+
+pp=pprint.PrettyPrinter(indent=4).pprint
 
 mylist = [1, 2, 7, 53, 90, 4, 80, 23, 7]
 
-print(mylist)
-
+pp(mylist)
+print("*"*80)
+print(" "*20, "Estimates of location")
+print("*"*80)
+print("===========statistics============")
 print(f"mean:{statistics.mean(mylist)}")
 print(f"fmean:{statistics.fmean(mylist)}")
 print(f"median:{statistics.median(mylist)}")
@@ -18,3 +26,15 @@ print(f"geometric_mean:{statistics.geometric_mean(mylist)}")
 print(f"median_high:{statistics.median_high(mylist)}")
 print(f"median_low:{statistics.median_low(mylist)}")
 print(f"hypop(ecludian distance from origin:{statistics.hypot(*mylist)}")
+print("===========scipy stats============")
+tm = stats.trim_mean(mylist, 0.2)
+print(f"stats.trim_mean:{tm}")
+print("*"*80)
+print(" "*20, "Estimates of variability")
+print("*"*80)
+print("===========pandas============")
+series = pd.Series(mylist)
+print(f"mean abs deviation(l1 norm, manhattan norm):{series.mad()}")
+print(f"variance, RMS, l2norm :{series.var()}")
+ax = series.plot.box(grid=True)
+ax.get_figure().savefig('box.png')
